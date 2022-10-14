b6e1892a0a4af7fd2e72fd1858c329845329296b
liendo1 <panaliendo@outlook.com>
2022-10-14 23:50:58 +0200

    adding the sounds

--- app/src/main/java/com/example/tatesmotivationapp/MainActivity.java
+++ app/src/main/java/com/example/tatesmotivationapp/MainActivity.java
@@ -2,14 +2,68 @@
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity {
+ public class MainActivity extends AppCompatActivity {
 
+    private ImageButton andrew;
+    private ImageButton tristan;
+    private MediaPlayer player;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setTitle("Choose your Hero!");
+
+        andrew = findViewById(R.id.andrew);
+        tristan = findViewById(R.id.talis);
+
+        andrew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play(v, R.raw.andrew_saying);
+            }
+        });
+
+
+        tristan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play(v,R.raw.talis);
+            }
+        });
+
     }
-}
+    public void play(View v, int nameMusic){
+        if (player == null){
+            player = MediaPlayer.create(this,nameMusic);
+            player.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    stopPlayer();
+                }
+            });
+        }
+        player.start();
+    }
+    public void stop(View v) {
+        stopPlayer();
+    }
+
+    private void stopPlayer() {
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
+     @Override
+     protected void onStop() {
+         super.onStop();
+         stopPlayer();
+     }
+ }
